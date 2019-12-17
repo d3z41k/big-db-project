dfcf7b8c6fded9ca3737121c6ff4c0d0c970c8d8
Denis Zhokhov <d3z41k@gmail.com>
2019-12-17 10:23:47 +0200

    Fix hello manager call

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Trade;
+use Graze\GuzzleHttp\JsonRpc\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -35,14 +36,25 @@ class HomeController extends Controller
      */
     public function trades()
     {
-        $trades = DB::table('trades')->limit(100000)->get();
+//        $trades = DB::table('trades')->limit(100000)->get();
+//
+//        $totalProfit = 0;
+//
+//        foreach ($trades as $trade) {
+//            $totalProfit += $trade->profit;
+//        }
 
-        $totalProfit = 0;
 
-        foreach ($trades as $trade) {
-            $totalProfit += $trade->profit;
+        $client = Client::factory('http://localhost:8082/rpc');
+
+        $body = $client->send($client->request(123, 'HelloService.SayHello', [['name' => 'Tom', 'age' => 27]]))->getBody();
+
+        while (!$body->eof()) {
+            $response = $body->read(1024);
         }
 
-        return view('trades', ['totalProfit' => $totalProfit]);
+        $data = json_decode($response);
+
+        return view('trades', ['totalProfit' => $data->result]);
     }
 }
